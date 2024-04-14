b7b856ce5a44bdda4cc86c7a809841e3e989d2e5
Situ Zhao <zzhaositu@gmail.com>
2024-04-14 04:45:29 -0700

    Update contentScript.js

--- youtube-bookmarker-finished-code-master/youtube-bookmarker-finished-code-master/contentScript.js
+++ youtube-bookmarker-finished-code-master/youtube-bookmarker-finished-code-master/contentScript.js
@@ -2,7 +2,7 @@
 document.body.addEventListener("mouseover", function(event) {
   //console.log(event);
   var elementext = event.target.innerText.toString();
-  console.log(elementext);
+  //console.log(elementext);
 // create an instance of the speech synthesis object
   chrome.runtime.sendMessage({toSay: elementext}, function() {});
 
@@ -16,7 +16,7 @@ document.body.addEventListener("mouseover", function(event) {
 });
 
 document.body.addEventListener("mouseout", function(event) {
-  console.log("messaged stopped 1");
+  //console.log("messaged stopped 1");
   chrome.runtime.sendMessage({toSay: "STOP the message"}, function() {});
 
 });
