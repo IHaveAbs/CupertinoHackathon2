d929e0ef044d7e10a2b9bb53bb3e145b02137f5c
Situ Zhao <zzhaositu@gmail.com>
2024-04-14 02:34:23 -0700

    Update background.js

--- youtube-bookmarker-finished-code-master/youtube-bookmarker-finished-code-master/background.js
+++ youtube-bookmarker-finished-code-master/youtube-bookmarker-finished-code-master/background.js
@@ -10,6 +10,12 @@ chrome.runtime.onMessage.addListener(function(request) {
   }
   
 });
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+  console.log("chrome tts stopped");
+  chrome.tts.stop();
+});
+
 /*chrome.tabs.onUpdated.addListener((tabId, tab) => {
   if (tab.url && tab.url.includes("youtube.com/watch")) {
     const queryParameters = tab.url.split("?")[1];
