12c5f55070c5eeb720ea71bb75753526416d1885
Situ Zhao <zzhaositu@gmail.com>
2024-04-14 06:09:39 -0700

    Update contentScript.js

--- youtube-bookmarker-finished-code-master/youtube-bookmarker-finished-code-master/contentScript.js
+++ youtube-bookmarker-finished-code-master/youtube-bookmarker-finished-code-master/contentScript.js
@@ -1,15 +1,17 @@
-
+var originalsize = 0;
 document.body.addEventListener("mouseover", function(event) {
   //send message to active text to speech
   var elementext = event.target.innerText.toString();
   chrome.runtime.sendMessage({toSay: elementext}, function() {});
+  originalsize = event.target.style.fontSize;
   event.target.style.fontSize = "24px";
+  
 });
 
 document.body.addEventListener("mouseout", function(event) {
   //stops the text to speech
   chrome.runtime.sendMessage({toSay: "STOP the message"}, function() {});
-
+  event.target.style.fontSize = originalsize;
 });
 
 
