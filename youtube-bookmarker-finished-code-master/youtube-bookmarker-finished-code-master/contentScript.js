d7c9c4089cb3d597b3962e6df9704f9c3bba54e4
Situ Zhao <zzhaositu@gmail.com>
2024-04-13 23:00:46 -0700

    Update contentScript.js

--- youtube-bookmarker-finished-code-master/youtube-bookmarker-finished-code-master/contentScript.js
+++ youtube-bookmarker-finished-code-master/youtube-bookmarker-finished-code-master/contentScript.js
@@ -3,5 +3,6 @@ console.log("fdslkafjldsakjfa");
 
 document.body.addEventListener("mouseover", function(event) {
   console.log(event);
-  
+  var elementext = event.target;
+  console.log(elementext)
 });
