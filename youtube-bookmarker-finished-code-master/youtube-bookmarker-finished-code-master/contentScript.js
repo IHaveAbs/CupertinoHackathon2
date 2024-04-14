3e7ec2c79752aa045e5783570520e8e352eb7f90
BR123BRqwrry <BrianRong2@gmail.com>
2024-04-13 23:26:10 -0700

    Update contentScript.js

--- youtube-bookmarker-finished-code-master/youtube-bookmarker-finished-code-master/contentScript.js
+++ youtube-bookmarker-finished-code-master/youtube-bookmarker-finished-code-master/contentScript.js
@@ -5,4 +5,8 @@ document.body.addEventListener("mouseover", function(event) {
   //console.log(event);
   var elementext = event.target.innerText;
   console.log(elementext)
+// create an instance of the speech synthesis object
 });
+function speak(elementext) {
+  chrome.tts.speak(elementext, { rate: 1.0 }); // Adjust rate as needed
+}
